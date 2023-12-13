9bdc0cb2ad1b2065a62e81a0bb3eac5f913c9442
GlassCactus <austin.h.kim@hotmail.com>
2023-12-13 03:09:15 -0800

    Better controls for moving the lights

--- src/Source.cpp
+++ src/Source.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c2c1b50f6876adb0e856a57f0bdf3f5ae22934543c993a283cbae5f1eea94d85
-size 21063
+oid sha256:56ec93ed744e94706c35af05586eb85f22605aaee6782ad8504151a43e6802d8
+size 21061
